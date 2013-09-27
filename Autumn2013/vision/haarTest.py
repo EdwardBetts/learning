68a3f8e9991f3d50052d2dc9cda3089db879884e
bork <bork@borkbox.(none)>
2013-09-27 15:59:09 +0200

    modified haarTest

--- Autumn2013/vision/haarTest.py
+++ Autumn2013/vision/haarTest.py
@@ -1,12 +1,15 @@
 import cv
 
-location = "D:\\Dropbox\\vision\\soniaImages\\"
-imgName = "a.png"
+location = "/home/bork/Data/learning/Autumn2013/vision/"
+imgName = "people.jpg"
 
 img = cv.LoadImage(location+imgName)
 out = cv.CreateMemStorage()
-haar = cv.LoadHaarClassifierCascade('haarcascade_frontalface_default.xml')
-allFaces = cv.HaarDetectObjects(image, haar, out, 1.2, 2, cv. CV_HAAR_DO_CANNY_PRUNING,(100,100))
+haar = cv.Load('/usr/share/opencv/haarcascades/haarcascade_frontalface_default.xml') #HaarClassifierCascade
+allFaces = cv.HaarDetectObjects(img, haar, out, 1.2, 2, cv. CV_HAAR_DO_CANNY_PRUNING,(100,100))
 if allFaces:
 	for face in allFaces:
 		print face
+		print 'hello'
+		cv.ShowImage(face)
+cv.waitKey(0)
