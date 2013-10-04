f417c79f5dd5dba1da60d5a709ab450351d84d43
debajyoti <dave0908@gmail.com>
2013-10-04 11:29:53 +0200

    added DummyTest

--- Autumn2013/vision/AdaProject/imagepreprocessing/src/image_preprocessing.adb
+++ Autumn2013/vision/AdaProject/imagepreprocessing/src/image_preprocessing.adb
@@ -1,3 +1,7 @@
 package body Image_Preprocessing is
    --function color to greyscale
+   function DummyTest return Boolean is
+   begin
+      return True;
+   end DummyTest;
 end Image_Preprocessing;
--- Autumn2013/vision/AdaProject/imagepreprocessing/src/image_preprocessing.ads
+++ Autumn2013/vision/AdaProject/imagepreprocessing/src/image_preprocessing.ads
@@ -1,3 +1,4 @@
 package Image_Preprocessing is
    --function color to greyscale
+   function DummyTest return Boolean;
 end Image_Preprocessing;
