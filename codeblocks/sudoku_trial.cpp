1ffeec3975498efec0f6b60ea6b5343d2f20c8f2
Debajyoti Nag <dave0908@gmail.com>
2014-10-04 01:11:05 +0200

    added difficulty classifier, began solver, added gitignore doc

--- codeblocks/sudoku_trial.cpp
+++ codeblocks/sudoku_trial.cpp
@@ -7,9 +7,11 @@ using namespace std;
 #define NY NX //square dimensions
 
 int grid[NX][NY]; //structure to store sudoku
+int blanks;
 
 int sRead(char fName[100])
 {
+    blanks = 0;
     ifstream sGrid;
     int a,b;
     string line;
@@ -29,8 +31,10 @@ int sRead(char fName[100])
                     continue;
                 int k = temp-'0';
                 if(k<1||k>9)
+                {
                     k = 0; //write 0(zero) in empty space
-
+                    blanks++;
+                }
                 grid[i][j] = k;
                 //cout<<k<<"\t";
             }
@@ -67,14 +71,41 @@ void sDisp()
 
 }
 
+int sClassify()
+{
+    //TODO Classify difficulty of sudoku
+    int c;
+    if(blanks > 30) c = 1;
+    else    if(blanks >25) c = 2;
+            else    if(blanks>21)   c=3;
+                    else    c = 4;          //Re-calibrate limits
+    switch(c)
+    {
+        case 1: return 1; break;    //easy
+        case 2: return 2; break;    //medium
+        case 3: return 3; break;    //hard
+        case 4: return 4;           //samurai
+    }
+}
+
+int sSolve()
+{
+    //TODO Solve sudoku
+
+}
+
 int main()
 {
     char fName[100]; //100 char name length
     cout<<"Enter full path of file to read (D:/codeblocks/sudoku.txt or sudoku.txt) :";
     cin>>fName;
     int t = sRead(fName);
-    if(t==1)
-        sDisp();
+    if(t!=1)
+        return 0;
+    sDisp();
+    int lvl = sClassify(); // to be called before solving
+    cout<<"\nDifficulty Level :"<<lvl;
+    sSolve();
 
     return 0;
 }
